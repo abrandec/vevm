04fc45bffeccd817f0aca5d675a1fc8dd2781e1d
e <abrandecarlo@protonmail.com>
2022-07-10 08:35:42 -0700

    added null char to logo

--- src/common/assets/assets.h
+++ src/common/assets/assets.h
@@ -1,7 +1,7 @@
 #ifndef ASSETS_H
 #define ASSETS_H
 
-const static char vevm_logo[81] =
-    "\033[7m\e[0;37mvevm \033[27m\033[93m▓▓\033[94m▓▓\033[92m▓▓\033[35m▓▓\033[91m▓▓\033[00m";
+const static char vevm_logo[82] =
+    "\033[7m\e[0;37mvevm \033[27m\033[93m▓▓\033[94m▓▓\033[92m▓▓\033[35m▓▓\033[91m▓▓\033[00m\0";
 
 #endif
